69c13c8ce2239bd3c5b95d4ccdaf3046eeda30af
Vivian Thach <vivianlthach@gmail.com>
2021-12-29 13:57:02 -0800

    Finish phase 4!

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,18 @@
 import {useEffect, useState} from 'react';
 
-function Message({ size }) {
+function Message({ size, featherCount }) {
     const [sizeClass, setSizeClass] = useState('');
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        if (featherCount <= 0){
+            setMessage('Oh my! Your bird is naked!');
+        } else if (featherCount >= 10) {
+            setMessage('Full turkey!');
+        } else {
+            setMessage('Coming along...')
+        }
+    }, [featherCount]);
 
     useEffect(() => {
         console.log('Message', size);
@@ -22,11 +33,11 @@ function Message({ size }) {
         }
         setSizeClass(cname);
         }, [size]);
-    
+
 
     return (
         <div className={`message ${sizeClass}`}>
-            (Oh my! Your bird is naked!)
+            {message}
         </div>
     );
 };
